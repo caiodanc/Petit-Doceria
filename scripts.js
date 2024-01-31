6fd0e0279e47db0314a3b127d91e817de38037f1
caiodanc <caiodanc@hotmail.com>
2024-01-31 20:53:55 -0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,62 @@
+const form = document.querySelector(".formulario-orçamento")
+
+const mascara = document.querySelector(".mascara-formulario")
+
+const checkgourmet = document.getElementById("gourmet")
+const gourmet = document.querySelector(".gourmet")
+
+const checkcap = document.getElementById("cappuccino")
+const cappuccino = document.querySelector(".cappuccino")
+
+const checkpist = document.getElementById("pistache")
+const pistache = document.querySelector(".pistache")
+
+const checkleite = document.getElementById("doceleite")
+const doceleite = document.querySelector(".doceleite")
+
+
+function showform() {
+
+  mascara.style.visibility = "visible"
+  form.style.left = "50%"
+  form.style.transform = "translateX(-50%)"
+}
+
+function hideform() {
+
+  form.style.left = "-300px"
+  mascara.style.visibility = "hidden"
+  mascara.style.transitionDelay = "0.5s";
+  gourmet.style.visibility = "hidden";
+  cappuccino.style.visibility = "hidden";
+  pistache.style.visibility = "hidden";
+  doceleite.style.visibility = "hidden";
+}
+
+function showqtd() {
+
+  if (checkgourmet.checked == true) {
+    gourmet.style.visibility = "visible";
+  } else {
+    gourmet.style.visibility = "hidden";
+  }
+
+  if (checkcap.checked == true) {
+    cappuccino.style.visibility = "visible";
+  } else {
+    cappuccino.style.visibility = "hidden";
+  }
+
+  if (checkpist.checked == true) {
+    pistache.style.visibility = "visible";
+  } else {
+    pistache.style.visibility = "hidden";
+  }
+
+  if (checkleite.checked == true) {
+    doceleite.style.visibility = "visible";
+  } else {
+    doceleite.style.visibility = "hidden";
+  }
+}
+
